05b39774c288c8eed273de29e84dfd626c627a35
Tyler Yahn <tyler@moonshadowmobile.com>
2015-08-11 15:58:11 -0700

    Update percent_mem_spec.rb.
    Update and alter existing tests to specify new desired behaviour of the
    percent_mem function.

--- spec/functions/percent_mem_spec.rb
+++ spec/functions/percent_mem_spec.rb
@@ -10,9 +10,12 @@ describe 'percent_mem' do
 
   it { should run.with_params(50).and_return('5g') }
   it { should run.with_params(0.1).and_return('256m') }
+  it { should run.with_params(0.1, '128m').and_return('128m') }
   it { should run.with_params(10.1).and_return('1g') }
+  it { should run.with_params(4).and_return('410m') }
   it { should run.with_params().and_raise_error(ArgumentError) }
   it { should run.with_params(-1).and_raise_error(Puppet::ParseError) }
   it { should run.with_params(101).and_raise_error(Puppet::ParseError) }
   it { should run.with_params('wrong').and_raise_error(ArgumentError) }
+  it { should run.with_params(40, 'wrong').and_raise_error(TypeError) }
 end
