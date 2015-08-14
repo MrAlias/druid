3ad23ab855b4085d55e21d1677c2510ae1c7da68
Tyler Yahn <tyler@moonshadowmobile.com>
2015-08-14 16:03:15 -0700

    Update druid unit tests.
    Add coverage of files managed by the druid class.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,6 +2,27 @@ require 'spec_helper'
 describe 'druid' do
 
   context 'with defaults for all parameters' do
+    it { should compile.with_all_deps }
     it { should contain_class('druid') }
+	it { should contain_file('/usr/local/lib/druid').with(
+		{
+			:ensure => :link,
+		}
+	)}
+	it { should contain_file('/usr/local/lib').with(
+		{
+			:ensure => :directory,
+		}
+	)}
+	it { should contain_file('/etc/druid').with(
+		{
+			:ensure => :directory,
+		}
+	)}
+	it { should contain_file('/etc/druid/common.runtime.properties').with(
+		{
+			:ensure => :file,
+		}
+	)}
   end
 end
