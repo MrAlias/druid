e67388ad4136e08f34f0a6046b0acba3ad46b1d1
Tyler Yahn <tyler@moonshadowmobile.com>
2015-08-18 13:41:25 -0700

    Update druid::historical smoke tests.
    Refine process sizing for small VMs expected to be used in testing.

--- tests/historical.pp
+++ tests/historical.pp
@@ -1,6 +1,8 @@
 class { 'druid::historical':
-  server_max_size              => 2147483648, 
-  processing_buffer_size_bytes => 100000000,
+  server_max_size              => 268435456,      # 256MB
+  processing_buffer_size_bytes => 134217728,      # 128MB
+  max_mem_allocation_pool      => '512m',
+  min_mem_allocation_pool      => '512m',
   processing_num_threads       => 1, 
   segment_cache_locations      => [
     {
