e7ecdfa105a98dfa730c4402f0aaa0712793dc14
Tyler Yahn <tyler@moonshadowmobile.com>
2015-08-19 17:26:11 -0700

    Update unit tests to new properties file location.

--- spec/classes/historical_spec.rb
+++ spec/classes/historical_spec.rb
@@ -1,34 +1,19 @@
 require 'spec_helper'
-describe 'druid::historical' do
-  context 'with defaults for all parameters' do
+describe 'druid::historical', :type => 'class' do
+  context 'On system with 10 GB RAM and defaults for all parameters' do
     let(:facts) do
       {
         :memorysize => '10 GB',
       }
     end
 
-    it { should compile.with_all_deps }
-    it { should contain_class('druid::historical') }
-    it { should contain_file('/etc/druid/runtime.properties').with(
-      {
-        :ensure => 'file',
-      }
-    )}
-    it { should contain_file('/etc/systemd/system/druid-historical.service').with(
-      {
-        :ensure => 'file',
-      }
-    )}
-    it { should contain_exec('Reload systemd daemon').with(
-      {
-        :command => '/bin/systemctl daemon-reload',
-      }
-    )}
-    it { should contain_service('druid-historical').with(
-      {
-        :ensure => 'running',
-        :enable => true,
-      }
-    )}
+    it {
+      should compile.with_all_deps
+      should contain_class('druid::historical')
+      should contain_file('/etc/druid/historical/runtime.properties').with({:ensure => 'file'})
+      should contain_file('/etc/systemd/system/druid-historical.service').with({:ensure => 'file'})
+      should contain_exec('Reload systemd daemon').with({:command => '/bin/systemctl daemon-reload'})
+      should contain_service('druid-historical').with({:ensure => 'running', :enable => true})
+    }
   end
 end
